e65abec364e1f5e9984a9b65ea7297121a11e211
fatmaomer <fatmaalzhraaomer2017@gmail.com>
2021-03-01 21:52:28 +0300

    firstCommit

---
+++ resources/lang/ar/admin/sidebar.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'totalsales' => 'إجمالي المبيعات',
+    'totalorder' => 'إجمالي الطلبات',
+    'productnumber' => 'عدد المنتجات',
+    'usernumber' => 'عدد العملاء',
+    'latestorder' => 'أحدث الطلبات',
+    'orderid' => 'رقم الأوردر',
+    'customer' => 'العميل',
+    'price' => 'السعر',
+    'orderstatus' => 'حالة الطلب',
+    'total' => 'الأجمالي',
+    'latestview' => 'أحدث التقيمات',
+    'product' => 'المنتج',
+    'review' => 'التقييم',
+    'settings' => 'الإعدادات',
+    'shipping' => 'وسائل التوصيل',
+    'freecharge' => 'توصيل مجاني',
+    'localpickup' => 'توصيل داخلي',
+    'flatrate' => 'توصيل خارجي',
+    'main' => 'الرئيسية ',
+    'editshipping' => ' تعديل وسيلة التوصيل',
+
+
+
+];
---
+++ resources/views/dashboard/settings/shippings/edit.blade.php
@@ -0,0 +1,194 @@
+@extends('layouts.admin')
+@section('content')
+
+    <div class="app-content content">
+        <div class="content-wrapper">
+            <div class="content-header row">
+            </div>
+            <div class="content-body">
+                <div id="crypto-stats-3" class="row">
+                    <div class="col-xl-3 col-12">
+                        <div class="card crypto-card-3 pull-up">
+                            <div class="card-content">
+                                <div class="card-body pb-0">
+                                    <div class="row">
+                                        <div class="col-2">
+                                            <h1><i class="cc BTC warning font-large-2" title="BTC"></i></h1>
+                                        </div>
+                                        <div class="col-5 pl-2">
+                                            <h4>{{__('dashboard.totalsales')}}</h4>
+
+                                        </div>
+                                        <div class="col-5 text-right">
+                                            <h4>$9,980</h4>
+                                            <h6 class="success darken-4">31% <i class="la la-arrow-up"></i></h6>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-12">
+                                        <canvas id="btc-chartjs" class="height-75"></canvas>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-12">
+                        <div class="card crypto-card-3 pull-up">
+                            <div class="card-content">
+                                <div class="card-body pb-0">
+                                    <div class="row">
+                                        <div class="col-2">
+                                            <h1><i class="cc ETH blue-grey lighten-1 font-large-2" title="ETH"></i></h1>
+                                        </div>
+                                        <div class="col-5 pl-2">
+                                            <h4>{{__('dashboard.totalorder')}}</h4>
+
+                                        </div>
+                                        <div class="col-5 text-right">
+                                            <h4>$944</h4>
+                                            <h6 class="success darken-4">12% <i class="la la-arrow-up"></i></h6>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-12">
+                                        <canvas id="eth-chartjs" class="height-75"></canvas>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-12">
+                        <div class="card crypto-card-3 pull-up">
+                            <div class="card-content">
+                                <div class="card-body pb-0">
+                                    <div class="row">
+                                        <div class="col-2">
+                                            <h1><i class="cc XRP info font-large-2" title="XRP"></i></h1>
+                                        </div>
+                                        <div class="col-5 pl-2">
+                                            <h4>{{__('dashboard.productnumber')}}</h4>
+
+                                        </div>
+                                        <div class="col-5 text-right">
+                                            <h4>$1.2</h4>
+                                            <h6 class="danger">20% <i class="la la-arrow-down"></i></h6>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-12">
+                                        <canvas id="xrp-chartjs" class="height-75"></canvas>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-12">
+                        <div class="card crypto-card-3 pull-up">
+                            <div class="card-content">
+                                <div class="card-body pb-0">
+                                    <div class="row">
+                                        <div class="col-2">
+                                            <h1><i class="cc XRP info font-large-2" title="XRP"></i></h1>
+                                        </div>
+                                        <div class="col-5 pl-2">
+                                            <h4>{{__('dashboard.usernumber')}}</h4>
+
+                                        </div>
+                                        <div class="col-5 text-right">
+                                            <h4>$1.2</h4>
+                                            <h6 class="danger">20% <i class="la la-arrow-down"></i></h6>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-12">
+                                        <canvas id="xrp-chartjs" class="height-75"></canvas>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Candlestick Multi Level Control Chart -->
+
+                <!-- Sell Orders & Buy Order -->
+                <div class="row match-height">
+                    <div class="col-12 col-xl-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h4 class="card-title">{{__('dashboard.latestorder')}}</h4>
+                                <a class="heading-elements-toggle"><i class="la la-ellipsis-v font-medium-3"></i></a>
+                                <div class="heading-elements">
+
+                                </div>
+                            </div>
+                            <div class="card-content">
+                                <div class="table-responsive">
+                                    <table class="table table-de mb-0">
+                                        <thead>
+                                        <tr>
+                                            <th>{{__('dashboard.orderid')}}</th>
+                                            <th>{{__('dashboard.customer')}}</th>
+                                            <th>{{__('dashboard.price')}}</th>
+                                            <th>{{__('dashboard.orderstatus')}}</th>
+                                            <th>{{__('dashboard.total')}}</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        <tr class="bg-success bg-lighten-5">
+                                            <td>10500</td>
+                                            <td>fatma omer </td>
+                                            <td>1205.5 kd</td>
+                                            <td>مكتمل</td>
+                                            <td>1889.5 kd</td>
+                                        </tr>
+
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-xl-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h4 class="card-title">{{__('dashboard.latestview')}}</h4>
+                                <a class="heading-elements-toggle"><i class="la la-ellipsis-v font-medium-3"></i></a>
+                                <div class="heading-elements">
+                                </div>
+                            </div>
+                            <div class="card-content">
+                                <div class="table-responsive">
+                                    <table class="table table-de mb-0">
+                                        <thead>
+                                        <tr>
+                                            <th>{{__('dashboard.customer')}}</th>
+                                            <th>{{__('dashboard.product')}}</th>
+                                            <th>{{__('dashboard.review')}}</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        <tr class="bg-danger bg-lighten-5">
+                                            <td>fatma omer</td>
+                                            <td> hand watch</td>
+                                            <td>5</td>
+                                        </tr>
+
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--/ Sell Orders & Buy Order -->
+                <!-- Active Orders -->
+
+                <!-- Active Orders -->
+            </div>
+        </div>
+    </div>
+    @stop
